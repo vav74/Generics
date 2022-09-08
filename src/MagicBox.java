181f4b945b9ff3b0ce3d29e64d3a8a85aace28fa
Vladislav Vinokurov <vav74@mail.ru>
2022-09-08 18:38:07 +0900

    Класс MagicBox

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -4,21 +4,27 @@ public class MagicBox<T> {
     private T[] items;
     private Random random = new Random();
 
-
-    public boolean add(T item){
- /* У вас должен быть метод добавления объекта boolean add(T item).
-    Если коробка ещё не полна, то элемент должен в ней сохраниться и метод вернуть true.
-    Если полна, то коробка не должна измениться, а метод должен вернуть false.*/
-        return true;
+    public MagicBox(int size) {
+        this.items = (T[]) new Object[size];
     }
 
-    public T pick(){
-/*  Также нужно реализовать метод T pick(), работающий следующим образом:
-    Если коробка не полна, то он выкидывает RuntimeException с сообщением, что коробка не полна
-    и осталось ещё столько-то ячеек заполнить.
-    Если коробка полна, то метод выбирает случайным образом (см. ниже)
-    один из сохранённых элементов и возвращает его из метода.*/
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
     }
 
-
+    public T pick() throws RuntimeException {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Коробка не полна,осталось заполнить " + (items.length - i) + " ячеек");
+            }
+        }
+        int randomInt = random.nextInt(items.length);
+        return items[randomInt];
+    }
 }
